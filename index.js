a87e99ac0dd9ed13d7bbbc02761f94dbc4e29cbd
Alessandro Oliveira <alessandro.oliveira@landisgyr.com>
2020-08-22 10:23:55 -0300

    Altera rota para teste2

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/teste', (req, res) => {
-    return res.json({ hello: 'World 2' });
+app.get('/teste2', (req, res) => {
+    return res.json({ hello: 'World 1' });
 });
 
 app.listen(3333);
